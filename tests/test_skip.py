7c57f2922e92edb9750fcbdc89753cf1d3a9f933
A-Prudnikova <a.prudnikova@gmail.com>
2022-08-16 13:56:11 +0300

    test_skip

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -3,23 +3,42 @@ from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 from selene import have
 
+url = 'https://github.com'
+sign_in = '[href="/login"'
+toggle_nav = 'button[aria-label="Toggle navigation"'
 
-@pytest.fixture()
-def desktop_fixture():
-    browser.open('https://github.com/').driver.maximize_window()
+chrome = pytest.fixture(params=[(460, 960), (1600, 992)])
 
-
-@pytest.fixture()
-def mobile_fixture():
-    browser.open('https://github.com/')
-
-
-def test_github_sign_in_desktop(desktop_fixture):
-    s('[href="/login"]').click()
-    s('.application-main ').should(have.text('Sign in to GitHub'))
+@chrome
+def browser_size(request):
+    return request
 
 
-def test_github_sign_in_mobile(mobile_fixture):
-    s('[aria-label="Toggle navigation"]').click()
-    s('[href="/login"]').click()
-    s('.application-main ').should(have.text('Sign in to GitHub'))
+@pytest.fixture(scope='function', autouse=True)
+def set_up_browser(browser_size):
+    width = browser_size.param[0]
+    height = browser_size.param[1]
+
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+    browser.open(url)
+
+
+def test_github_sign_in_desktop():
+    if browser._config.window_width < 1024:
+        pytest.skip('Size for mobile version')
+
+    s(sign_in).click()
+    s('h1').should(have.exact_text('Sign in to GitHub'))
+
+
+
+def test_github_sign_in_mobile():
+    if browser._config.window_width > 1023:
+        pytest.skip('Size for desktop version')
+
+    s(toggle_nav).click()
+    s(sign_in).click()
+    s('h1').should(have.exact_text('Sign in to GitHub'))
+
