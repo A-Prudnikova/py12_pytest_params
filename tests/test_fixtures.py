0fcbc6dbff30fea89a12fb6af363e2a0d9bfdfa3
A-Prudnikova <a.prudnikova@gmail.com>
2022-08-19 19:37:12 +0300

    после проверки

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -3,37 +3,33 @@ from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 from selene import have
 
-'''try1'''
-
 url = 'https://github.com'
 sign_in = '[href="/login"'
 toggle_nav = 'button[aria-label="Toggle navigation"'
 
-chrome = pytest.fixture(params=[(460, 960), (1600, 992)])
 
-@chrome
-def browser_size(request):
-    return request
-
-
-@pytest.fixture(scope='function', autouse=True)
-def set_up_browser(browser_size):
-    width = browser_size.param[0]
-    height = browser_size.param[1]
-
-    browser.config.window_width = width
-    browser.config.window_height = height
+@pytest.fixture(scope='function', params=[(1600, 992)])
+def set_up_browser_desktop(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
 
     browser.open(url)
 
-@pytest.mark.parametrize("browser_size", [(1600, 992)], indirect=True)
-def test_github_desktop_indirect(browser_size):
+
+def test_github_desktop_parametrize(set_up_browser_desktop):
     s(sign_in).click()
     s('h1').should(have.exact_text('Sign in to GitHub'))
 
 
-@pytest.mark.parametrize("browser_size", [(460, 960)], indirect=True)
-def test_github_mobile_indirect(browser_size):
+@pytest.fixture(scope='function', params=[(460, 960)])
+def set_up_browser_mobile(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+
+    browser.open(url)
+
+
+def test_github_mobile_parametrize(set_up_browser_mobile):
     s(toggle_nav).click()
     s(sign_in).click()
     s('h1').should(have.exact_text('Sign in to GitHub'))
